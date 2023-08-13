1d533e6b6bbbee83a0b50c2e40d639cb53ac7bf5
Mel <meljohnpualon@gmail.com>
2023-08-13 11:37:22 +0800

    fix navbar state

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('app running')
+const navbar = document.querySelector('.navbar')
+
+const checkScrollValue = () => {
+  if (window.scrollY > 80) {
+    navbar.classList.add('navbar-active')
+  } else {
+    navbar.classList.remove('navbar-active')
+  }
+}
+
+
+window.addEventListener('scroll', checkScrollValue)
